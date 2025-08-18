d8624f69ffc0bdbbbba4e92897f33607e2f15a5e
Misteranshu <163726815+Misteranshu@users.noreply.github.com>
2025-08-18 15:41:24 +0530

    Update backend for Render deployment

--- server.js
+++ server.js
@@ -23,12 +23,18 @@ app.use('/api/v1/coursepurchase', webhookattach);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+cors({
+  origin: "*",
+  credentials: true,
+})
+
 
 app.use('/api/v1/user', userrouter);
 app.use('/api/v1/uploadmedia', Uploadrouter);
